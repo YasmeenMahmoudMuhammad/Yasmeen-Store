3d7b1baa27ec4954222e2cc664e9444799cc3549
yasmin <y.mahmoud>
2023-02-25 21:42:48 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RiAncientGateFill,  RiHome2Fill, RemixIconModule} from 'angular-remix-icon';
+import { FooterComponent } from './footer/footer.component';
 const icons = {
   RiAncientGateFill,
   RiHome2Fill,
@@ -31,7 +32,8 @@ const icons = {
     CheckOutComponent,
     NavbarComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
